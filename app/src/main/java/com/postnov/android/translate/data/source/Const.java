6a3a9ad419efc436488a79b3a8d0873eb9ec82aa
valpostnov <valpostnov@gmail.com>
2016-07-24 18:11:02 +0300

    Delete Const.java

--- app/src/main/java/com/postnov/android/translate/data/source/Const.java
+++
@@ -1,8 +0,0 @@
-package com.postnov.android.translate.data.source;
-
-/**
- * Created by platon on 24.07.2016.
- */
-public class Const {
-    private static final String ENDPOINT = "https://dictionary.yandex.net/api/v1/dicservice.json/";
-}
